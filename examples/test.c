069a95658fedd566d5e40d5e642179cc3c795a57
System Administrator <root@localhost.localdomain>
2020-03-21 03:29:48 +0300

    add predefined macros

--- examples/test.c
+++ examples/test.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World\n");
 #if defined(__LCC__)
 #  if defined(__e2k__) /* MCST eLbrus C Compiler */
     printf("MCST eLbrus C Compiler\n");
@@ -9,5 +8,18 @@ int main() {
     printf("Local (or Little) C Compiler\n");
 #  endif
 #endif
+
+// #if 5 <= __GNUC__ && !defined __ICC && !(defined __LCC__ && __LCC__ <= 123)
+//#  warning Recomend use a LCC >= 1.23 version
+// #elif 5 <= __GNUC__ && !defined __ICC && \
+//         !(defined __LCC__ && __LCC__ <= 123) && !__STRICT_ANSI__
+//#  error LCC must be 1.23 version
+// #endif
+
+// https://gcc.gnu.org/onlinedocs/cpp/Standard-Predefined-Macros.html
+    printf("Message from file: %s\nMessage from line: %d\nIn function: %s\nCompiled: %s %s\n", __FILE__, __LINE__, __FUNCTION__, __DATE__, __TIME__);
+    printf("is STD:%d\nSTD ver: %d\nLCC ver: %d\nis E2K Arch: %d\n", __STDC__, __STDC_VERSION__, __LCC__, __e2k__);
+    //printf("%d %d\n", , __ASSEMBLER__, __cplusplus);
+
     return 0;
 }
